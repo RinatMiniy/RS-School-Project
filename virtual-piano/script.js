0ffe2e3e12f181309ee2e7b4e988eb9e33fc06de
RinatMiniy <rinatminiy@gmail.com>
2021-03-18 17:00:09 +0300

    feat: func play sound

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -1,18 +1,32 @@
 const piano = document.querySelector(".piano");
 const pianoKeys = document.querySelectorAll(".piano-key");
-window.addEventListener('keydown', function(e){
-  const audio = document.querySelector(`audio[data-key="${e.code}"]`)
+
+window.addEventListener('keydown', function(event){
+  const audio = document.querySelector(`audio[data-key="${event.code}"]`);
+  const key = document.querySelector(`div[data-key="${event.code}"]`);
   if(!audio) return;
+  key.classList.add('active');
   playAudio(audio)
 });
 
 piano.addEventListener('click', (event) => {
   if(event.target.classList.contains('piano-key')) {
-    playAudio(src);
+    const key = event.target.dataset.key;
+    audio = document.querySelector(`audio[data-key="${key}"]`);
+    event.target.classList.add('active');
+    event.target.classList.add('active::before');
+    playAudio(audio);
   }   
 });
 
 function playAudio(audio) {
   audio.currentTime = 0;
   audio.play();
+}
+
+const keys = Array.from(document.querySelectorAll('.piano-key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+function removeTransition (event) {
+  event.target.classList.remove("active")
 }
